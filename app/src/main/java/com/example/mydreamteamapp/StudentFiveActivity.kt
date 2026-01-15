9390f8468c3b13255aed7343468d0bf8f532360e
johnalrei <johnalrei.dt@gmail.com>
2026-01-15 15:32:25 +0800

    student five

--- app/src/main/java/com/example/mydreamteamapp/StudentFiveActivity.kt
+++ app/src/main/java/com/example/mydreamteamapp/StudentFiveActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,6 +29,7 @@ fun StudentFive(name: String, description: String) {
     val name = "Keith Lawrenz M. Abalos"
     val description = "I am a 3rd year student currently studying in Pamantasan ng Cabuyao and taking " +
             "a Bachelor of Science in Information Technology"
+    val imageId = R.drawable.profile
 
     Column(
         modifier = Modifier
@@ -35,6 +38,14 @@ fun StudentFive(name: String, description: String) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = imageId),
+            contentDescription = "Student Image", // Provide description for accessibility
+            modifier = Modifier.height(150.dp) // Adjust the size as needed
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         Text(
             text = name,
             fontSize = 28.sp,
