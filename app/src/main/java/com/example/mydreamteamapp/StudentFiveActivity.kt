56eaf7b8788c4762accc4cd55203ca5928fa82cd
johnalrei <johnalrei.dt@gmail.com>
2026-01-15 15:12:13 +0800

    student five

--- app/src/main/java/com/example/mydreamteamapp/StudentFiveActivity.kt
+++ app/src/main/java/com/example/mydreamteamapp/StudentFiveActivity.kt
@@ -1,2 +1,51 @@
 package com.example.mydreamteamapp
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mydreamteamapp.ui.theme.MyDreamTeamAppTheme
+
+@Composable
+fun StudentFive(name: String, description: String) {
+
+    val name = "Keith Lawrenz M. Abalos"
+    val description = "I am a 3rd year student currently studying in Pamantasan ng Cabuyao and taking " +
+            "a Bachelor of Science in Information Technology"
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = name,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = description,
+            fontSize = 16.sp,
+        )
+    }
+}
